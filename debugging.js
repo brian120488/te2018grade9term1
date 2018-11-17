ea726edb158ace836724711d930a96aacdbf36ce
brian120488 <brian.yang@csedge.org>
2018-11-17 11:28:17 -0800

    Finished debugging.js

--- debugging.js
+++ debugging.js
@@ -1,38 +1,43 @@
 // Remove block comments when ready to debug a program.
 
 // Program #1: "Careless"
-/*
-let x == 3;
-if(x => 3 {
-  X+++;
-}} Else {
-  X =+ 2;
 
-console.log = X;
-*/
-
-// Program #2: "Bad logic and math"
-/*
-let money = 28;
-if(money > 50 && money <= 20) {
-  money /= 2;
-} else {
-  money /= 0;
+let x = 5;
+if(x >= 3) {
+  x++;
+} 
+else if(x < 3) {
+x = x + 2;
 }
-*/
 
-// Program #3: "And what else?"
-/*
+console.log(`${x}`);
+
+
+//Program #2: "Bad logic and math"
+
+let money = 9;
+if(money < 50 && money >= 20) {
+  money = money / 2;
+} else {
+  money = money / 3;
+}
+
+console.log(`${money}`);
+
+
+//Program #3: "And what else?"
+const READLINE = require("readline-sync");
+
 let num1 = READLINE.question("Enter num1: ");
 let num2 = READLINE.question("Enter num2: ");
 let num3 = READLINE.question("Enter num3: ");
-if(num1 > num2) {
+if(num1 > num2 && num1 > 3) {
   console.log(`${num1} is the largest value.`);
 }
-else if(num2 > num3) {
+else if(num2 > num3 && num2 > 3) {
   console.log(`${num2} is the largest value.`);
 }
 else {
   console.log(`${num3} is the largest value.`);
 }
-*/
+
